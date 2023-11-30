2ce47896b4f3e9a5bff47f14545228f02c40ffd0
Civilian <renounce774@naver.com>
2023-11-30 14:25:59 +0900

    변수

---
+++ src/ch01_variables/Ex01_Variables.java
@@ -0,0 +1,24 @@
+package ch01_variables;
+
+public class Ex01_Variables {
+
+	public static void main(String[] args) {
+		//본인의 이름, 나이,  키 , 주소, 전화번호를 입력할 수 있는 변수를 각각 선언해보기
+		String name = "김메가";
+		String address = "디저트삼 구";
+		String mobile = "010-6677-1515" ;
+		double age = 24.5;
+		double height = 161.4;
+		
+		System.out.println("성명: " + name);
+		System.out.println("주소: " + address);
+		System.out.println("번호: " + mobile);
+		System.out.println("이름: " + age);
+		System.out.println("신장: " + height);
+		System.out.println("알까 말까 MGC커피~");
+			
+	}
+		
+		
+		
+}
---
+++ src/ch01_variables/memem.java
@@ -0,0 +1,18 @@
+package ch01_variables;
+
+public class memem {
+
+	public static void main(String[] args) {
+		
+		String name = "김메가";
+		String address = "디저트 삼구";
+		String mobile = "010-6677-1515" ;
+		double age = 24.5;
+		double height = 161.4;
+		
+		System.out.println("이름 : " + name  + "\t"  +  "나이 : "+age  + "\t"  +"키 : " 
+				+height +"\n" + " 주소 : "+address+ "\t"+"전화번호 : "+mobile );
+
+	}
+
+}
