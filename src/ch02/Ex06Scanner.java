313db5446d6da16b424550ea1c8062bbbe2fb429
Civilian <renounce774@naver.com>
2023-12-01 11:33:01 +0900

    'scanner1'

--- src/ch02/Ex06Scanner.java
+++ src/ch02/Ex06Scanner.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class Ex06Scanner {
 
 	public static void main(String[] args) {
+		// 정수를 입력받아 num1 이라는 변수에 저장
+		// 우변: scanner 클래스 객체가 가지고 있는 nextInt() 메서드를 호출
+		// 좌변: 호출결과를 num1에 대입
 		
 		Scanner scanner = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요: ");
+		int num1 = scanner.nextInt();
+		System.out.println(num1);
 		
-		
-		
-		
-		
-
 	}
 
 }
---
+++ src/ch02/Ex07_Calculator.java
@@ -0,0 +1,69 @@
+package ch02;
+
+import java.util.Scanner;
+
+public class Ex07_Calculator {
+
+	public static void main(String[] args) {
+		
+		// 정수 2개를 각각 스캐너로 num1, num2 에 입력받아서 입력받은 수의 합을 출력
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" 숫자를 입력해주세요");
+		int num1 = sc.nextInt();
+		System.out.println("다음 숫자를 입력해주세요");
+		int num2 = sc.nextInt();
+		
+		int result = num1 + num2;
+		System.out.println(num1 + " + " + num2 + " = " + result);
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		Scanner scn = new Scanner(System.in);
+		
+		String text1 = scn.next();
+		int sum1 = scn.nextInt();
+		double sum2 = scn.nextDouble();
+		String text2 = scn.nextLine();
+		boolean scr = scn.nextBoolean();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
