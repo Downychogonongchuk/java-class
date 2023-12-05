3a35d49ee13e61be130019a701e3f61e43a90144
Civilian <renounce774@naver.com>
2023-12-05 16:51:57 +0900

    for별찍기 피라미드 시도

--- src/ch06_for/Ex11_For2.java
+++ src/ch06_for/Ex11_For2.java
@@ -2,6 +2,14 @@ package ch06_for;
 
 public class Ex11_For2 {
     public static void main(String[] args) {
+
+        for (int i=1; i<=5; i++){ // 줄 수 , 별을 출력할 횟수
+            for (int j =(i+1); j <= 5 ; j++){ // 별을 줄수만큼 출력
+                System.out.print(" ");
+            }
+            System.out.println("*"); // 줄을 바꾸기 위한 용도  ㅇㅅㅇ
+        }
+
         for (int i=1; i<=5; i++){ // 줄 수 , 별을 출력할 횟수
             for (int j =1; j <= i ; j++){ // 별을 줄수만큼 출력
                 System.out.print("*");
@@ -9,4 +17,8 @@ public class Ex11_For2 {
             System.out.println(" "); // 줄을 바꾸기 위한 용도  ㅇㅅㅇ
         }
     }
-}
+    }
+
+
+
+
