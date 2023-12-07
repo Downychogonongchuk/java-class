4a6cea94efba9c45401bd02e6c0a8119713ce946
Civilian <renounce774@naver.com>
2023-12-07 16:29:32 +0900

    배열 숫자 위치 찾기

--- src/ch09_arraybasic/Ex_06.java
+++ src/ch09_arraybasic/Ex_06.java
@@ -7,7 +7,7 @@ public class Ex_06 {
         int num[] = {5, 2, 1, 3, 4, 7, 6, 9, 10, 8};
         /**
          * 위와 같은 배열이 있을 때 1~10 사이의 숫자 중에서 하나를
-         * 입력했을 때 그 숫자가 몇 번 인덱스에 있는지 출력ㄴ
+         * 입력했을 때 그 숫자가 몇 번 인덱스에 있는지 출력
          */
         Scanner sc = new Scanner(System.in);
         System.out.println("찾으시는 숫자를 입력해주세요");
