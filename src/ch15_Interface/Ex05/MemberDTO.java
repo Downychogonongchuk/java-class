25402c2ed80888d982378cc1cba10e5509c37865
Civilian <renounce774@naver.com>
2023-12-27 10:36:36 +0900

    인터페이스

---
+++ src/ch15_Interface/Ex05/MemberDTO.java
@@ -0,0 +1,81 @@
+package ch15_Interface.Ex05;
+
+/** 설하언니가 연말이니까 다같이 술마시자고 밥방 멤 모아본다고 했는데 ㄱㄴ?
+ * 12월 금요일 언젠가 ㄱㄴ?
+ *
+ *
+ */
+
+
+public class MemberDTO {
+    private Long id;
+    private String memberEmail;
+    private String memberPassword;
+    private String memberName;
+    private String memberMobile;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+
+    public String getMemberPassword() {
+        return memberPassword;
+    }
+
+    public void setMemberPassword(String memberPassword) {
+        this.memberPassword = memberPassword;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberMobile() {
+        return memberMobile;
+    }
+
+    public void setMemberMobile(String memberMobile) {
+        this.memberMobile = memberMobile;
+    }
+
+    public MemberDTO() {
+
+    }
+
+    private static Long idValue = 1L;
+
+    public MemberDTO(String memberEmail, String memberPassword, String memberName, String memberMobile) {
+        this.id = idValue++;
+        this.memberEmail = memberEmail;
+        this.memberPassword = memberPassword;
+        this.memberName = memberName;
+        this.memberMobile = memberMobile;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberDTO{" +
+                "id=" + id +
+                ", memberEmail='" + memberEmail + '\'' +
+                ", memberPassword='" + memberPassword + '\'' +
+                ", memberName='" + memberName + '\'' +
+                ", memberMobile='" + memberMobile + '\'' +
+                '}';
+    }
+}
